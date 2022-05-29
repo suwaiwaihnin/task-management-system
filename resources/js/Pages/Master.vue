c2457e0f70540c64e267cbfc884827788aebcd72
Su Wai Wai Hnin <47345199+suwaiwaihnin@users.noreply.github.com>
2022-05-29 19:08:03 +0630

    Update Master.vue

--- resources/js/Pages/Master.vue
+++ resources/js/Pages/Master.vue
@@ -64,7 +64,7 @@
             </a>
             <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
               <div class=" dropdown-header noti-title">
-                <h6 class="text-overflow m-0">Welcome!</h6>
+                <h6 class="text-overflow m-0">Welcome {{$page.props.auth.name}}!</h6>
               </div>
               <div class="dropdown-divider"></div>
               <inertia-link href="/logout" class="dropdown-item">
