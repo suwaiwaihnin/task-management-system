957aafa1e7b3a1ceab3f67624521a29df1879d7d
Su Wai Wai Hnin <47345199+suwaiwaihnin@users.noreply.github.com>
2022-05-29 18:59:33 +0630

    Update Master.vue

--- resources/js/Pages/Master.vue
+++ resources/js/Pages/Master.vue
@@ -57,12 +57,9 @@
           <li class="nav-item dropdown">
             <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <div class="media align-items-center">
-                <span class="avatar avatar-sm rounded-circle">
-                  <img alt="Image placeholder" :src="'/assets/img/theme/team-4-800x800.jpg'">
-                </span>
-                <div class="media-body ml-2 d-none d-lg-block">
-                  <span class="mb-0 text-sm  font-weight-bold">{{$page.props.auth.name}}</span>
-                </div>
+                    <div class="media-body ml-2 d-none d-lg-block">
+                        <span class="mb-0 text-sm  font-weight-bold">{{$page.props.auth.name}}</span>
+                    </div>
               </div>
             </a>
             <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
